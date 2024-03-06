c2e66880a8fdf11b24fc6e21cc22dd41ab7cc270
shivangi-gupta-01 <110471751+shivangi-gupta-01@users.noreply.github.com>
2024-03-06 21:33:49 +0530

    Task

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Home } from './components/HomePage/Home';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { MainContainer } from './components/MainContainer/MainContainer';
 import { Ticket } from './components/Ticket/Ticket';
+import { TaskScreen } from './components/TaskScreen/TaskScreen';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<MainContainer/>}>
             <Route path='home' element={<Home />} /> {/*works as main/home */}
             <Route path='ticket' element={<Ticket/>} />
+            <Route path='task' element={<TaskScreen/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
